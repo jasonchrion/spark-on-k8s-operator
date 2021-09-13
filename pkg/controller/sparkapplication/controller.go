f6b3986ff5e09c2ebbc4cf9fbaf0eadce38f1701
jasonchrion <jasonchrion@gmail.com>
2021-09-13 13:59:03 +0800

    Export HADOOP_CONF_DIR during spark-submit in operator pod by copying hadoop site xmls present in hadoopConfigMap to operator pod.
    https://github.com/nokia/spark-on-k8s-operator/pull/4
    https://github.com/GoogleCloudPlatform/spark-on-k8s-operator/issues/572

--- pkg/controller/sparkapplication/controller.go
+++ pkg/controller/sparkapplication/controller.go
@@ -445,6 +445,8 @@ func (c *Controller) handleSparkApplicationDeletion(app *v1beta2.SparkApplicatio
 	// SparkApplication deletion requested, lets delete driver pod.
 	if err := c.deleteSparkResources(app); err != nil {
 		glog.Errorf("failed to delete resources associated with deleted SparkApplication %s/%s: %v", app.Namespace, app.Name, err)
+		// SparkApplication deletion requested, lets delete directory where hadoopConfigMap site xmls were copied.
+		config.RemoveDirectory(app.Namespace, app.Name)
 	}
 }
 
@@ -665,7 +667,7 @@ func (c *Controller) submitSparkApplication(app *v1beta2.SparkApplication) *v1be
 
 	driverPodName := getDriverPodName(app)
 	submissionID := uuid.New().String()
-	submissionCmdArgs, err := buildSubmissionCommandArgs(app, driverPodName, submissionID)
+	exportEnvVars, submissionCmdArgs, err := buildSubmissionCommandArgs(app, driverPodName, submissionID)
 	if err != nil {
 		app.Status = v1beta2.SparkApplicationStatus{
 			AppState: v1beta2.ApplicationState{
@@ -678,7 +680,7 @@ func (c *Controller) submitSparkApplication(app *v1beta2.SparkApplication) *v1be
 		return app
 	}
 	// Try submitting the application by running spark-submit.
-	submitted, err := runSparkSubmit(newSubmission(submissionCmdArgs, app))
+	submitted, err := runSparkSubmit(exportEnvVars, newSubmission(submissionCmdArgs, app))
 	if err != nil {
 		app.Status = v1beta2.SparkApplicationStatus{
 			AppState: v1beta2.ApplicationState{
@@ -936,6 +938,7 @@ func (c *Controller) recordSparkApplicationEvent(app *v1beta2.SparkApplication) 
 			"SparkApplication %s was submitted successfully",
 			app.Name)
 	case v1beta2.FailedSubmissionState:
+		config.RemoveDirectory(app.Namespace, app.Name)
 		c.recorder.Eventf(
 			app,
 			apiv1.EventTypeWarning,
@@ -944,6 +947,7 @@ func (c *Controller) recordSparkApplicationEvent(app *v1beta2.SparkApplication) 
 			app.Name,
 			app.Status.AppState.ErrorMessage)
 	case v1beta2.CompletedState:
+		config.RemoveDirectory(app.Namespace, app.Name)
 		c.recorder.Eventf(
 			app,
 			apiv1.EventTypeNormal,
@@ -951,6 +955,7 @@ func (c *Controller) recordSparkApplicationEvent(app *v1beta2.SparkApplication) 
 			"SparkApplication %s completed",
 			app.Name)
 	case v1beta2.FailedState:
+		config.RemoveDirectory(app.Namespace, app.Name)
 		c.recorder.Eventf(
 			app,
 			apiv1.EventTypeWarning,
@@ -959,6 +964,7 @@ func (c *Controller) recordSparkApplicationEvent(app *v1beta2.SparkApplication) 
 			app.Name,
 			app.Status.AppState.ErrorMessage)
 	case v1beta2.PendingRerunState:
+		config.RemoveDirectory(app.Namespace, app.Name)
 		c.recorder.Eventf(
 			app,
 			apiv1.EventTypeWarning,
